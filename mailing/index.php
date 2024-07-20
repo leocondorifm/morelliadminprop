c35485fa0d68daeeb9effd2fad1ada6439faf4a4
LeoCondoriFM <leocondori.fm@gmail.com>
2024-07-20 13:22:14 -0300

    servicio mailing

---
+++ mailing/index.php
@@ -0,0 +1,37 @@
+<?php
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Selective\BasePath\BasePathMiddleware;
+    use Psr\Http\Message\ResponseInterface;
+    use Slim\Exception\HttpNotFoundException;
+    use Slim\Factory\AppFactory;
+    use Selective\BasePath\BasePathDetector;
+
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $app = AppFactory::create();
+
+    // Add Slim routing middleware
+    $app->addRoutingMiddleware();
+
+
+    $app->setBasePath("/mailing");
+
+    $app->addErrorMiddleware(true, true, true);
+
+    // Define app routes
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write("Hello, world!");
+        return $response;
+    });
+
+    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+        $name = $args['name'];
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    });
+
+     // Run app
+     $app->run();
+
+?>
