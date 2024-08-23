eaa39191ada5e61e1b38f9d0da3c4b31c144327a
LeoCondoriFM <leocondori.fm@gmail.com>
2024-08-23 10:11:24 -0300

    pagos

--- view/template.php
+++ view/template.php
@@ -24,8 +24,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Administración Diego Morelli">
+    <meta name="author" content="Desarrolló LeoCondori.com.ar">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="manifest" href="../site.webmanifest">
+    <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 
     <title>Morelli - Admin</title>
     <base href="<?php echo $ruta_api;?>" />
